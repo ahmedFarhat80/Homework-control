c1387cd86ffc3f05d22790cd703812d64abfeb27
Ahmed Mohamed Ahmed Farhat <72593329+ahmedFarhat80@users.noreply.github.com>
2020-11-12 00:22:41 +0200

    first commit

---
+++ resources/views/tasck.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ahmed Farhat</title>
+</head>
+
+<body>
+
+<ul>
+@foreach ($tascks as $key => $tasck)
+ <li> <h3> <a href="{{'tasck/show/' . $key}}"> {{$tasck}} </a> </h3>  </li>   
+@endforeach
+</ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 //لازم اعرف هاد الاوبجكت
@@ -14,7 +13,6 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -23,7 +21,6 @@ Route::get('/', function () {
 
 Route::get('ahmed', function () {
     $name='Ahmed Farhat';
-
     return view('ahmed',compact('name'));
 });
 
@@ -31,3 +28,32 @@ Route::post('send',function(Request $request) {
     $name = $request->name;
     return view('ahmed',compact('name'));
 });
+
+
+
+Route::get('tasck', function () {
+    $tascks = [ 
+       '1'  => 'tasck 1'
+       ,
+       '2'  =>'tasck 2'
+       ,
+       '3'  =>'tasck 3'
+    ];
+    return view('tasck' , compact('tascks'));
+});
+
+
+
+Route::get('tasck/show/{id}', function ($id) {
+    $tascks = [ 
+       '1'  => 'tasck 1'
+       ,
+       '2'  =>'tasck 2'
+       ,
+       '3'  =>'tasck 3'
+    ];
+
+    $tasck = $tascks[$id];
+    return view('show' , compact('tasck'));
+});
+
